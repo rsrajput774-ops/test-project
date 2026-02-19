9e34fa9e4456eb07b55c9e045e80efaf60ecebda
ram.singh <ram.singh@theninehertz.com>
2026-02-19 10:40:54 +0530

    initial commit

---
+++ logic.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    return a + b;
+}
+
+let total = sum(5, 6);
+
+// Show in console
+console.log("sum", total);
+
+// Show in HTML
+document.getElementById("result").innerText = "Sum is: " + total;
